56c47c644bdc02067ed3e0ea7c2cdbf2e5e053a6
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2025-06-17 01:16:07 +0530

    hadhing and verifying the password

---
+++ backend/internal/utils/hashing/hashing.go
@@ -0,0 +1,16 @@
+package hashing
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPassword(hashedPassword string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
