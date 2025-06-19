e55ebd059d3af7e53832c70a09ee21dd2558bb63
harash <harash.poriya@gmail.com>
2025-06-19 11:13:55 +0530

    add pointer

--- backend/internal/storage/storage.go
+++ backend/internal/storage/storage.go
@@ -4,5 +4,5 @@ import "github.com/sudhanshu042004/sandbox/internal/types"
 
 type Storage interface {
 	CreateUser(name string, email string, password string) (int64, error)
-	GetUser(email string) (types.User, error)
+	GetUser(email string) (*types.User, error)
 }
